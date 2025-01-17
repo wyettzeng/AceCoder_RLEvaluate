8eb77cbf7ad619846af43e9dc0066785f2c4ada3
wyettzeng <wyettzeng@gmail.com>
2025-01-17 15:17:31 +0000

    updates

---
+++ setup.sh
@@ -0,0 +1,5 @@
+
+mkdir inferenced_output
+# creating symbolic links
+ln -s /data/code_llm/evalplus/inferenced_output/rl_results/ inferenced_output/evalplus
+ln -s /data/code_llm/bigcodebench/bcb_results/ inferenced_output/bcb_results
