b67ec6754aa17c7269333a9ab8a4ac49de34416b
wyettzeng <wyettzeng@gmail.com>
2025-01-17 16:46:23 +0000

    updates

--- analyze_rl_results.py
+++ analyze_rl_results.py
@@ -1,27 +1,29 @@
-from typing import List
-
 import pandas as pd
 
 from eval_dataset.constants import ALL_EVAL_DATASET
 
-MODELS = [
-    "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1",
-    "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1/checkpoint-1000",
-    "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1/checkpoint-1600",
-]
+MODELS = {
+    "01_17": "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1",
+    "01_17_chkp_1000": "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1/checkpoint-1000",
+    "01_17_chkp_1600": "trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1/checkpoint-1600",
+}
 
 
-def report_rl_results(models: List[str]):
+def report_rl_results():
     results = []
     for dataset_name, dataset in ALL_EVAL_DATASET.items():
-        for model in models:
+        for short_model_name, model_path in MODELS.items():
             try:
-                acc = dataset.load_accuracy(model)
+                acc = dataset.load_accuracy(model_path)
                 results.append(
-                    {"model": model, "dataset": dataset_name, "accuracy": acc}
+                    {
+                        "model": short_model_name,
+                        "dataset": dataset_name,
+                        "accuracy": acc,
+                    }
                 )
             except Exception as e:
-                print(f"failed to retrieve {dataset_name} + {model}")
+                print(f"failed to retrieve {dataset_name} + {short_model_name}")
                 # raise e
 
     df = pd.DataFrame(results)
@@ -32,4 +34,4 @@ def report_rl_results(models: List[str]):
 
 
 if __name__ == "__main__":
-    report_rl_results(MODELS)
+    report_rl_results()
