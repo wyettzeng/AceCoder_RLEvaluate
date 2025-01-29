597c7595310cb6653b0b29d6b87cefc04c6a1dc7
wyettzeng <wyettzeng@gmail.com>
2025-01-29 00:05:56 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ mkdir inferenced_output
 # creating symbolic links
 ln -s /data/code_llm/evalplus/inferenced_output/rl_results/ inferenced_output/evalplus
 ln -s /data/code_llm/bigcodebench/bcb_results/ inferenced_output/bcb_results
+ln -s /data/code_llm/LiveCodeBench/output inferenced_output/livecodebench
 
 # Create conda environment
 conda create -n codeRLEval python=3.11
